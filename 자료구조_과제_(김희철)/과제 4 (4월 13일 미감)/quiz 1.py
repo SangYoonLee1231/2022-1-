ffc933eab336021d36fd593e89c9773fcd76a8e6
SangYoonLee <sylagape1231@gmail.com>
2022-04-08 00:39:08 +0900

    Update All
    2022-04-08
    - 자료구조 과제

---
+++ 자료구조_과제_(김희철)/과제 4 (4월 13일 미감)/quiz 1.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.link = None
+
+class LinkedStack:
+    def __init__(self):
+        self.top = None
+    
+    def isEmpty(self):
+        return self.top == None
+    
+    def push(self, val):
+        newNode = Node(val)
+        newNode.link = self.top
+        self.top = newNode
+    
+    def pop(self):
+        if self.isEmpty == None:
+            return "Empty"
+        e = self.top.data
+        self.top = self.top.link
+        return e
+
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div_quo(a, b):
+    return a // b
+
+def div_rem(a, b):
+    return a % b
+
+
+def func(postfix_lst):
+    for elem in postfix_lst:
+        if type(elem) is int:
+            ls.push(elem)
+        else:
+            op = elem
+            opr2 = ls.pop()
+            opr1 = ls.pop()
+
+            if opr2 == "Empty" or opr1 == "Empty":
+                return None
+
+            ls.push(math_func[op](opr1, opr2))
+    
+    ans = ls.pop()
+
+    if ls.isEmpty:
+        return ans
+    else:
+        return None
+
+
+math_func = {
+    '+' : add,
+    '-' : sub,
+    '*' : mul,
+    '//' : div_quo,
+    '%' : div_rem
+}
+
+
+ls = LinkedStack()
+postfix_lst = list(input().split())
+
+ans = func(postfix_lst)
+
+if ans == None:
+    print("Stack is Empty")
+else:
+    print(ans)
+    
---
+++ 자료구조_과제_(김희철)/과제 4 (4월 13일 미감)/quiz 2.py
@@ -0,0 +1,46 @@
+class Queue:
+    MAX_QSIZE = 100
+    def __init__(self):
+        self.list = [None] * Queue.MAX_QSIZE
+        self.front = -1
+        self.rear =-1
+        self.size = 0
+
+    def isEmpty(self):
+        return self.size == 0
+    
+    def isFull(self):
+        # 큐가 full일 경우 리스트 길이를 2배로 확장
+        if self.list.size < self.MAX_QSIZE:
+            # 리스트 용량이 넉넉한 경우
+            print()
+
+            # 정상적으로 enqueue 과정 수행
+
+        else: # A.n == A.capacity # 리스트 용량이 더 필요한 경우 (자동으로 용량 크기 ↑)
+            n = self.size()
+            list_temp = [None] * Queue.MAX_QSIZE * 2 # B는 임시용 리스트, *2는 용량을 늘린다는 의미
+
+            for i in range(n):
+                list_temp[i] = self.list[i]
+            self.list = list_temp
+            del list_temp
+            self.list[n] = item
+            self.list.n = n + 1
+
+
+    def enqueue(self, item):
+        self.isFull()
+
+
+        
+    def dequeue(self):
+        self.isEmpty()
+    
+    def peek(self):
+        self.isEmpty()
+
+    def size(self):
+        return self.size()
+        
+    def clear():
